2e00edd4c4d41d509c17e3148036ac3e3ecfda49
Con-Ramos <132285553+Con-Ramos@users.noreply.github.com>
2024-10-13 18:18:23 -0300

    Update mux_always.v

--- mux_always.v
+++ mux_always.v
@@ -1,12 +1,12 @@
 module mux_always #(parameter integer WIDTH = 1)(  // Modulo mux realiza a selecao dos dados
     input wire [WIDTH-1:0] in_A,            // Entrada 0, in_A = primeira entrada do mux
     input wire [WIDTH-1:0] in_B,            // Entrada 1, in_B = segunda entrada do mux
-    input wire SEL,                         // Sinal de selecao, qual das duas entradas sera passada para a saida
+    input wire sel,                         // Sinal de selecao, qual das duas entradas sera passada para a saida
     output reg[WIDTH-1:0] mux_output  );    // A saida do mux
 
     always @(*) 
         begin
-            if (SEL == 1)                   //Se SEL for 1(true) a saida sera igual a in_B
+            if (sel == 1)                   //Se sel for 1(true) a saida sera igual a in_B
                 mux_output = in_B;
             else                            //Se nao, (false) a saida sera igual a in_A
                 mux_output = in_A;
