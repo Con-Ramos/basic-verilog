8798af990ee45593e97cf1325eb704d0d335d8b9
Con-Ramos <132285553+Con-Ramos@users.noreply.github.com>
2025-08-15 00:10:07 -0300

    Update mux_always.v

--- mux_always.v
+++ mux_always.v
@@ -1,14 +1,14 @@
-module mux_always #(parameter integer WIDTH = 1)(  // Modulo mux realiza a selecao dos dados
-    input wire [WIDTH-1:0] in_A,            // Entrada 0, in_A = primeira entrada do mux
-    input wire [WIDTH-1:0] in_B,            // Entrada 1, in_B = segunda entrada do mux
-    input wire sel,                         // Sinal de selecao, qual das duas entradas sera passada para a saida
-    output reg[WIDTH-1:0] mux_output  );    // A saida do mux
+module mux_always #(parameter integer WIDTH = 1)(  //modulo mux realiza a selecao dos dados
+    input wire [WIDTH-1:0] in_A,            //entrada 0, in_A = primeira entrada do mux
+    input wire [WIDTH-1:0] in_B,            //entrada 1, in_B = segunda entrada do mux
+    input wire sel,                         //sinal de selecao, qual das duas entradas sera passada para a saida
+    output reg[WIDTH-1:0] mux_output  );    //saida do mux
 
-    always @(*) 
+    always @(*) //executa sempre que qualquer uma das entradas de um bloco combinacional ou sequencial muda de valor
         begin
-            if (sel == 1)                   //Se sel for 1(true) a saida sera igual a in_B
+            if (sel == 1)                   //se sel for 1(true) a saida sera igual a in_B
                 mux_output = in_B;
-            else                            //Se nao, (false) a saida sera igual a in_A
+            else                            //se nao, (false) a saida sera igual a in_A
                 mux_output = in_A;
         end
-endmodule                                   //Indica o final do modulo
+endmodule                                   //final do modulo
